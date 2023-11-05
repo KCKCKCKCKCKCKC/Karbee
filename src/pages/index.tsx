41da7f09891712450c95998e3af333a61856e05f
Casey Li <caseyli0417@gmail.com>
2023-11-05 15:57:33 -0800

    started on grid for main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Navbar from '../components/navbar/navbar';
 import Banner from '../components/banner/banner';
 import styles from './home/home.module.css';
+import ArtCard from '../components/art-card/art-card';
 import Head from 'next/head';
  
 /* Entry point for app. Homepage */
@@ -17,6 +18,11 @@ export default function Home() {
           <Banner />
         </div>
         <h1>Explore</h1>
+        <p>Use article cards from mantine ui to construct the grid layout
+          Use first article card w/ image for each card and 
+          Articles cards grid for filling it with data and grid layout
+        </p>
+        <ArtCard />
       </main>
     </>
   );
