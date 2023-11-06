7c894d4787949ea08fe7dc6398026046598e7f7c
Casey Li <caseyli0417@gmail.com>
2023-11-06 00:42:22 -0800

    Fixed navbar positioning

--- src/components/light-dark-button/light-dark-button.tsx
+++ src/components/light-dark-button/light-dark-button.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, useMantineColorScheme, useComputedColorScheme } from '@mantine/core';
+import { ActionIcon, useMantineColorScheme, useComputedColorScheme, Tooltip } from '@mantine/core';
 import { IconSun, IconMoon } from '@tabler/icons-react';
 import cx from 'clsx';
 import classes from './light-dark-button.module.css';
@@ -8,16 +8,18 @@ function LightDarkButton() {
   const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
 
   return (
-    <ActionIcon
-      onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
-      variant="gradient"
-      size="lg"
-      aria-label="Toggle Light Dark"
-      gradient={{ from: 'rgba(255, 85, 0, 1)', to: 'yellow', deg: 174 }}
-    >
-      <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
-      <IconMoon className={cx(classes.icon, classes.dark)} stroke={1.5} />
-    </ActionIcon>
+    <Tooltip label="Toggle Mode">
+      <ActionIcon
+        onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
+        variant="gradient"
+        size="lg"
+        aria-label="Toggle Light Dark"
+        gradient={{ from: 'rgba(255, 85, 0, 1)', to: 'yellow', deg: 174 }}
+      >
+        <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
+        <IconMoon className={cx(classes.icon, classes.dark)} stroke={1.5} />
+      </ActionIcon>
+    </Tooltip>
   );
 }
 
